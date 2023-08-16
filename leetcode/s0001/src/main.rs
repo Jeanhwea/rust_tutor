3578e3f1661edd2451bf5af41ee7b8fe02c3365c
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-16 22:17:44 +0800

    add

---
+++ leetcode/s0001/src/main.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+struct Solution {}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::with_capacity(nums.len());
+
+        for i in 0..nums.len() {
+            if let Some(k) = map.get(&(target - nums[i])) {
+                if *k != i {
+                    return vec![*k as i32, i as i32];
+                }
+            }
+            map.insert(nums[i], i);
+        }
+        panic!("not found!");
+    }
+}
+
+fn main() {
+    let nums = vec![3, 2, 4];
+    let target = 6;
+    let ans = Solution::two_sum(nums, target);
+    println!("ans = {:?}", ans);
+}
