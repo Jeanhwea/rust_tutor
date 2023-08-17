997ab465403bf77700101f532beae4503c58700b
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-17 15:19:30 +0800

    timestamp: 2023-08-17 15:19:30

---
+++ example/t08_control_flow/src/s02_loop.rs
@@ -0,0 +1,36 @@
+#![allow(unreachable_code)]
+
+fn main() {
+    // nesting and labels
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            // This would break only the inner loop
+            //break;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+}
+
+fn break_for_loop() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+}
---
+++ example/t08_control_flow/src/s04_for_range.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // 从 1 到 100 (不包含 100)
+    for n in 1..100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    // 从 1 到 100 (包含 100)
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+}
