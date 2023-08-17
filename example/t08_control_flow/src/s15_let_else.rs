3cbe2c3c956566232b4d257409bdc1bbf6d06eb5
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-17 16:23:57 +0800

    timestamp: 2023-08-17 16:23:57

--- example/t08_control_flow/src/s15_let_else.rs
+++ example/t08_control_flow/src/s15_let_else.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 
+// let-else 语句
 fn get_count_item(s: &str) -> (u64, &str) {
     let mut it = s.split(' ');
 
@@ -9,12 +10,14 @@ fn get_count_item(s: &str) -> (u64, &str) {
     let Ok(count) = u64::from_str(count_str) else {
         panic!("Can't parse integer: '{count_str}'");
     };
+
     (count, item)
 }
 
-assert_eq!(get_count_item("3 chairs"), (3, "chairs"));
+// 注意 let-else 语句和 match, if-let 的不同点在用变量的 scope
+fn get_count_item_2(s: &str) -> (u64, &str) {
+    let mut it = s.split(' ');
 
-fn main() {
     let (count_str, item) = match (it.next(), it.next()) {
         (Some(count_str), Some(item)) => (count_str, item),
         _ => panic!("Can't segment count item pair: '{s}'"),
@@ -24,4 +27,11 @@ fn main() {
     } else {
         panic!("Can't parse integer: '{count_str}'");
     };
+
+    (count, item)
+}
+
+fn main() {
+    assert_eq!(get_count_item("3 chairs"), (3, "chairs"));
+    assert_eq!(get_count_item_2("3 chairs"), (3, "chairs"));
 }
