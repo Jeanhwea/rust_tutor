367ca5d46fb9c0dbe0e7f653f215392725e2ffbf
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-21 11:33:28 +0800

    update: 完成标准库的使用

--- example/t19_std_library_types/src/s12_arc.rs
+++ example/t19_std_library_types/src/s12_arc.rs
@@ -11,6 +11,8 @@ fn main() {
         // reference in the memory heap.
         let apple = Arc::clone(&apple);
 
+        // 这里 move 关键字表示闭包函数中的变量采用所有权，而不是借用
+        // 达到的效果时在其生命周期外任然可以使用变量 apple
         thread::spawn(move || {
             // As Arc was used, threads can be spawned using the value allocated
             // in the Arc variable pointer's location.
