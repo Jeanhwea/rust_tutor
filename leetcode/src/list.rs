7baf17ff7fdadd19517f162c3e360a5d7ff1cb23
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-23 09:59:12 +0800

    update

--- leetcode/src/list.rs
+++ leetcode/src/list.rs
@@ -10,4 +10,39 @@ impl ListNode {
     pub fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
+
+    pub fn from_vec(vec: Vec<i32>) -> Option<Box<ListNode>> {
+        vec_to_list(vec)
+    }
+
+    pub fn into_vec(list: Option<Box<ListNode>>) -> Vec<i32> {
+        let mut rest = list;
+        std::iter::from_fn(move || {
+            if let Some(node) = rest.as_mut() {
+                let val = Some(node.val);
+                rest = node.next.take();
+                val
+            } else {
+                None
+            }
+        })
+        .collect()
+    }
+}
+
+pub fn vec_to_list(vec: Vec<i32>) -> Option<Box<ListNode>> {
+    vec.into_iter()
+        .rev()
+        .fold(None, |next, val| Box::new(ListNode { val, next }).into())
+}
+
+#[macro_export]
+macro_rules! list {
+    () => {None};
+    ($elem:expr; $n:expr) => {
+        $crate::ListNode::from_vec(vec![$elem; $n])
+    };
+    ($($x:expr),+ $(,)?) => {
+        $crate::ListNode::from_vec(vec![$($x),+])
+    };
 }
