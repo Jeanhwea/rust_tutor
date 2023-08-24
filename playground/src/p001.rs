12bb8cf10f37c17cda2a1df5835639ec69a73224
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-24 18:47:19 +0800

    feat: 添加打印信息

--- playground/src/p001.rs
+++ playground/src/p001.rs
@@ -1,3 +1,15 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
 fn main() {
-    println!("Hello");
+    let jack = &Person {
+        name: "Jack".to_string(),
+        age: 23,
+    };
+    println!("Hello, Person = {:?}", jack);
 }
